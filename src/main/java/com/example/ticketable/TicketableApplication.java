1a0070045427b2dc19ee6ca0a118754262e3a97a
cheoni5417 <cheoni5417@naver.com>
2025-04-02 10:53:14 +0900

    init commit

---
+++ src/main/java/com/example/ticketable/TicketableApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketableApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TicketableApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/ticketable/TicketableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketableApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
