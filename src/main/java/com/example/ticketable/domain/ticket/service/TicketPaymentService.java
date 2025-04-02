7bc396137af2956929b692f97735590bfc58f1f7
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-04-02 14:20:40 +0900

    ✨ Feat: ticket 공통 코드 추가

---
+++ src/main/java/com/example/ticketable/domain/ticket/service/TicketPaymentService.java
@@ -0,0 +1,10 @@
+package com.example.ticketable.domain.ticket.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TicketPaymentService {
+
+}
