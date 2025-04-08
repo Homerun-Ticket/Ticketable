2f2987e63babdd72f3e5d2205b7125b8a26518f2
cheoni5417 <cheoni5417@naver.com>
2025-04-08 22:38:42 +0900

    ✨ Feat: PG사 연동을 통한 결제 시스템 생성 (아직 미완성)

---
+++ src/main/java/com/example/ticketable/domain/point/controller/PointPaymentController.java
@@ -0,0 +1,32 @@
+package com.example.ticketable.domain.point.controller;
+
+import com.example.ticketable.common.entity.Auth;
+import com.example.ticketable.domain.point.entity.PointPayment;
+import com.example.ticketable.domain.point.service.PointPaymentService;
+import com.siot.IamportRestClient.response.IamportResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class PointPaymentController {
+	
+	private final PointPaymentService pointPaymentService;
+	
+	@PostMapping("/v1/payments/{imp_uid}")
+	public ResponseEntity<IamportResponse<PointPayment>> iamPortPayment(
+		@AuthenticationPrincipal Auth auth,
+		@PathVariable Long imp_uid
+	) {
+		log.info("imp_uid = {}", imp_uid);
+		return ResponseEntity.ok(pointPaymentService.iamPortPayment(auth, imp_uid));
+	}
+}
