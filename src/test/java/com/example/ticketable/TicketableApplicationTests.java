79e8ecbf1fa9273f0d96387308445630ccad1f05
AirIHL <inhak1122@gmail.com>
2025-04-10 21:08:50 +0900

    🐛 Fix: 테스트 환경에서 application.properties의 설정이 로드되지 않도록 설정

--- src/test/java/com/example/ticketable/TicketableApplicationTests.java
+++ src/test/java/com/example/ticketable/TicketableApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class TicketableApplicationTests {
 
 	@Autowired
